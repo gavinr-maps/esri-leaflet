622460af3fc5e75fe3d9b91d5861d8628ac87ef1
Gavin Rehkemper <137905994+gavinr-maps@users.noreply.github.com>
2026-06-24 09:35:56 -0500

    Update src/Layers/TiledMapLayer.js
    Co-authored-by: Simon MᶜKenzie <17579442+simonmckenzie@users.noreply.github.com>

--- src/Layers/TiledMapLayer.js
+++ src/Layers/TiledMapLayer.js
@@ -89,8 +89,8 @@ export const TiledMapLayer = TileLayer.extend({
           // try lod map first, then just default to zoom level
           z:
             this._lodMap &&
-            this._lodMap[zoom] !== undefined &&
             this._lodMap !== "crs"
+            this._lodMap[zoom] !== undefined &&
               ? this._lodMap[zoom]
               : zoom,
         },
